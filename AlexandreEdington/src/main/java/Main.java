0ee5caa169aa0b7140c6e3e44f915e6824f75845
Alexandreae <alex.almeida.1510@gmail.com>
2018-02-28 13:35:55 -0300

    commit2

--- AlexandreEdington/src/main/java/Main.java
+++ AlexandreEdington/src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.io.*;
 
 public class Main {
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws IOException {
 		    System.out.println("Projeto 1");
+		    
+		    String string;
+		    
+		    @SuppressWarnings("resource")
+			BufferedReader br = new BufferedReader(new FileReader("teste.txt"));
+		    while ((string = br.readLine()) != null) {
+		    	StringBuilder newString = new StringBuilder(string);
+		    	int x =0;
+		    	while (x < newString.length()){
+		    		if (newString.charAt(x) == '#') newString.setCharAt(x, 'X');
+		    		x++;
+		    	}
+		    	System.out.println(newString);
+		    }
 		  }
 }
